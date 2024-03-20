e88bdccebeb5a9e53a403c83ed7cc1ed56a3ff43
Alex Hoerler <alex.hoerler@gatech.edu>
2024-03-19 20:48:00 -0400

    Finish Dataset, add trainTest file

--- chineseEnglishDataset.py
+++ chineseEnglishDataset.py
@@ -1,27 +1,55 @@
 import pickle
 import torch
 from torch.utils.data import Dataset
+from transformers import BertTokenizer
+
+engBertTokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+chinBertTokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
 
 # Define Dataset
 class ChineseEnglishDataset(Dataset):
-    def __init__(self, englishChinesePickle, switchTransform=False):
+    def __init__(self, englishChinesePickle, switchTransform=False, englishTokenizer=engBertTokenizer, chineseTokenizer=chinBertTokenizer, max_length=25):
         eng_chin_dict = dict()
         with open(englishChinesePickle, "rb") as pickle_file:
             eng_chin_dict = pickle.load(pickle_file)
         
-        self.switchSeqLabel = switchTransform
+        self.switchSeqTarget = switchTransform
+        if not self.switchSeqTarget:
+            self.seqTokenizer = englishTokenizer
+            self.targetTokenizer = chineseTokenizer
+        else:
+            self.seqTokenizer = chineseTokenizer
+            self.targetTokenizer = englishTokenizer
+        self.max_length = max_length
+
         self.sequences = []
-        self.labels = []
-        for key_number in eng_chin_dict:
-            mappings = eng_chin_dict[key_number]
-            self.sequences.append(mappings[0])
-            self.labels.append(mappings[1])
+        self.targets = []
+
+        if not self.switchSeqTarget:
+            for key_number in eng_chin_dict:
+                mappings = eng_chin_dict[key_number]
+                self.sequences.append(mappings[0])
+                self.targets.append(mappings[1])
+        else:
+            for key_number in eng_chin_dict:
+                mappings = eng_chin_dict
+                self.sequences.append(mappings[1])
+                self.targets.append(mappings[0])
     
     def __len__(self):
-        return len(self.labels)
+        return len(self.targets)
     
     def __getitem__(self, index):
-        if not self.switchSeqLabel:
-            return self.sequences[index], self.labels[index]
+        seq_tokens = self.seqTokenizer(self.sequences[index])
+        if len(seq_tokens) > self.max_length:
+            seq_tokens = seq_tokens[: self.max_length - 1] + seq_tokens[-1: ]
         else:
-            return self.labels[index], self.sequences[index]
+            seq_tokens = seq_tokens + [0] * (self.max_length - len(seq_tokens))
+        
+        target_tokens = self.targetTokenizer(self.targets[index])
+        if len(target_tokens) > self.max_length:
+            target_tokens = target_tokens[: self.max_length - 1] + target_tokens[-1: ]
+        else:
+            target_tokens = target_tokens + [0] * (self.max_length - len(target_tokens))
+        
+        return torch.tensor(seq_tokens), torch.tensor(target_tokens)
---
+++ trainTestModel.py
@@ -0,0 +1,4 @@
+import torch
+from torch.utils.data import DataLoader
+from chineseEnglishDataset import *
+
