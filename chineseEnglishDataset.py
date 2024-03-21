dde5e58e2647d5a10a80552d66a913b0e3a068ba
Alex Hoerler <alex.hoerler@gatech.edu>
2024-03-21 03:53:11 -0400

    Fix some Dataset max_length, Finish train function

--- chineseEnglishDataset.py
+++ chineseEnglishDataset.py
@@ -8,7 +8,7 @@ chinBertTokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
 
 # Define Dataset
 class ChineseEnglishDataset(Dataset):
-    def __init__(self, englishChinesePickle, switchTransform=False, englishTokenizer=engBertTokenizer, chineseTokenizer=chinBertTokenizer, max_length=25):
+    def __init__(self, englishChinesePickle, switchTransform=False, englishTokenizer=engBertTokenizer, chineseTokenizer=chinBertTokenizer, eng_max_length=20, chin_max_length=20):
         '''
         Initialize Dataset
         Params:
@@ -24,11 +24,14 @@ class ChineseEnglishDataset(Dataset):
         self.switchSeqTarget = switchTransform
         if not self.switchSeqTarget:
             self.seqTokenizer = englishTokenizer
+            self.seq_max_length = eng_max_length
             self.targetTokenizer = chineseTokenizer
+            self.target_max_length = chin_max_length
         else:
             self.seqTokenizer = chineseTokenizer
+            self.seq_max_length = chin_max_length
             self.targetTokenizer = englishTokenizer
-        self.max_length = max_length
+            self.target_max_length = eng_max_length
 
         self.sequences = []
         self.targets = []
@@ -36,13 +39,15 @@ class ChineseEnglishDataset(Dataset):
         if not self.switchSeqTarget:
             for key_number in eng_chin_dict:
                 mappings = eng_chin_dict[key_number]
-                self.sequences.append(mappings[0])
-                self.targets.append(mappings[1])
+                if len(mappings[0]) < 55 and len(mappings[1]) < 25:
+                    self.sequences.append(mappings[0])
+                    self.targets.append(mappings[1])
         else:
             for key_number in eng_chin_dict:
                 mappings = eng_chin_dict
-                self.sequences.append(mappings[1])
-                self.targets.append(mappings[0])
+                if len(mappings[0]) < 55 and len(mappings[1]) < 25:
+                    self.sequences.append(mappings[1])
+                    self.targets.append(mappings[0])
     
     def __len__(self):
         return len(self.targets)
@@ -50,16 +55,16 @@ class ChineseEnglishDataset(Dataset):
     def __getitem__(self, index):
         # Get source tokens list and fit to max_length size
         seq_tokens = self.seqTokenizer(self.sequences[index])
-        if len(seq_tokens) > self.max_length:
-            seq_tokens = seq_tokens[: self.max_length - 1] + seq_tokens[-1: ]
+        if len(seq_tokens) > self.seq_max_length:
+            seq_tokens = seq_tokens[: self.seq_max_length - 1] + seq_tokens[-1: ]
         else:
-            seq_tokens = seq_tokens + [0] * (self.max_length - len(seq_tokens))
+            seq_tokens = seq_tokens + [0] * (self.seq_max_length - len(seq_tokens))
         
         # Get target tokens list and fit to max_length size
         target_tokens = self.targetTokenizer(self.targets[index])
-        if len(target_tokens) > self.max_length:
-            target_tokens = target_tokens[: self.max_length - 1] + target_tokens[-1: ]
+        if len(target_tokens) > self.target_max_length:
+            target_tokens = target_tokens[: self.target_max_length - 1] + target_tokens[-1: ]
         else:
-            target_tokens = target_tokens + [0] * (self.max_length - len(target_tokens))
+            target_tokens = target_tokens + [0] * (self.target_max_length - len(target_tokens))
         
         return torch.tensor(seq_tokens), torch.tensor(target_tokens)
--- trainTestModel.py
+++ trainTestModel.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 from chineseEnglishDataset import *
+from transformer import Transformer
 
 def createDataloaders(train_percentage=0.9, eng_source=True, batch_size=32, shuffle=True):
     if eng_source:
@@ -16,3 +17,65 @@ def createDataloaders(train_percentage=0.9, eng_source=True, batch_size=32, shuf
     eval_data_loader = DataLoader(eval_subset, batch_size=batch_size)
     return train_data_loader, eval_data_loader
 
+def trainModel(model, optimizer, criterion, data_loader, iteration_num):
+    model.train()
+    total_loss = 0
+    
+    print(f"--- Iteration {iteration_num} ---")
+    for batch_num, batch in enumerate(data_loader):
+        input_seq, target_seq = batch[0].to(device), batch[1].to(device)
+        X, y = input_seq, target_seq
+
+        for output_idx in range(1, y.shape[1]):
+
+            # skip if all values to be predicted are padded values
+            if torch.all(y[:, output_idx] == 0):
+                break
+
+            # filter all sequences already filtered (only padding left)
+            finished_filter = y[:, output_idx] != 0
+            y = y[finished_filter]
+            X = X[finished_filter]
+
+            # get input to decoder and true label
+            y_input = y[:, :output_idx]
+            y_expected = y[:, output_idx]
+            
+            # calculate masks
+            src_pad_mask = model.create_pad_mask(X, pad_token=0).to(device)
+            y_input_length = y_input.shape[1]
+            tgt_mask = model.get_tgt_mask(y_input_length).to(device)
+
+            pred = model(X, y_input, tgt_mask, src_pad_mask)
+            
+            pred = pred.permute(1, 2, 0)
+            loss = criterion(pred, y_expected)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            loss_item = loss.detach().item()
+            total_loss += loss_item
+
+        if batch_num % 100000 == 0:
+            print(f"Batch {batch_num}, Loss: {loss_item}")
+    
+    average_loss = total_loss / len(data_loader)
+    print(f"--- Iteration {iteration_num} - Average Loss: {average_loss} ---")
+    return model
+
+if __name__ == "__main__":
+    train_data_loader, eval_data_loader = createDataloaders()
+    eng_vocab_size = engBertTokenizer.vocab_size
+    chin_vocab_size = chinBertTokenizer.vocab_size
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = Transformer(input_vocab_size=eng_vocab_size, output_vocab_size=chin_vocab_size).to(device)
+    optimizer = torch.optim.Adam(model.parameters())
+    criterion = torch.nn.CrossEntropyLoss()
+    epochs = 10
+
+    for epoch_num in range(epochs):
+        model = trainModel(model, optimizer, criterion, train_data_loader)
+        torch.save(model.state_dict(), "transformer.pth")
